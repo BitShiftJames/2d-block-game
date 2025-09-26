1107e9941d5e207cb118b48e29126f0da6207d37
JamesWoolbright <j.robert.woolbright@gmail.com>
2025-09-26 13:58:25 -0400

    Added really simplistic tilemap generation and rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
 #include "raylib.h"
 #include "jamTypes.h"
+struct tile {
+  u32 type;
+  // shader information
+  u32 light;
+};
+struct world {
+  // maximum world size is 65,535
+  u16 Width;
+  u16 Height;
+  u16 TileWidth;
+  u16 TileHeight;
+  tile *map;
+};
 
 int main() {
   i32 ScreenWidth = 800;
@@ -13,26 +26,59 @@ int main() {
   Texture2D bubbleTexture = LoadTextureFromImage(bubbleTest);
   UnloadImage(bubbleTest);
 
-  // passing in 0 tells raylib to load default.
+  // passing in 0 tells raylib to load default. 
   Shader testShader = LoadShader(0, "../shaders/lighting.frag");
+
+  world global_world = {0};
+  global_world.Width = 500;
+  global_world.Height = 200;
+  global_world.TileWidth = 16;
+  global_world.TileHeight = 16;
+  global_world.map = (tile *)MemAlloc(sizeof(tile) * global_world.Width * global_world.Height);
+
+  for (i32 tileY = 0; tileY < global_world.Height; tileY++) {
+    for (i32 tileX = 0; tileX < global_world.Width; tileX++) {
+      tile CurrentTile = global_world.map[tileY * global_world.Width + tileX];
+      if (tileY > global_world.TileHeight / 2) {
+        if (tileX % 2 == 1) {
+          CurrentTile.type = 1;
+        } else {
+          CurrentTile.type = 2;
+        }
+      }
+
+      global_world.map[tileY * global_world.Width + tileX] = CurrentTile;
+    }
+  }
 
   while (!WindowShouldClose()) {
 
     BeginDrawing();
 
       ClearBackground(GRAY);
-      
-      DrawText("First window", 30, 30, 20, RED);
 
-        DrawTexture(bubbleTexture, 
-                    ScreenWidth / 2 - ((bubbleTexture.width / 2) * 4), 
-                    ScreenHeight / 2 - bubbleTexture.height / 2, WHITE);
+      for (i32 tileY = 0; tileY < global_world.Height; tileY++) {
+        for (i32 tileX = 0; tileX < global_world.Width; tileX++) {
+          tile CurrentTile = global_world.map[tileY * global_world.Width + tileX];
+          switch (CurrentTile.type) {
+            case 1: {
+              DrawRectangle(tileX * global_world.TileWidth, tileY * global_world.TileHeight, 
+                          global_world.TileWidth, global_world.TileHeight, RED);
+            } break;
+            case 2: {
+              DrawRectangle(tileX * global_world.TileWidth, tileY * global_world.TileHeight, 
+                          global_world.TileWidth, global_world.TileHeight, BLUE);
+            }
+          }
+        }
+      }
 
-      BeginShaderMode(testShader);
-        DrawTexture(bubbleTexture, 
-                    ScreenWidth / 2 - bubbleTexture.width / 2, 
-                    ScreenHeight / 2 - bubbleTexture.height / 2, WHITE);
-      EndShaderMode();
+// shader work later
+//      BeginShaderMode(testShader);
+//        DrawTexture(bubbleTexture, 
+//                    ScreenWidth / 2 - bubbleTexture.width / 2, 
+//                    ScreenHeight / 2 - bubbleTexture.height / 2, WHITE);
+//      EndShaderMode();
 
     EndDrawing();
 
